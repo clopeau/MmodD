8a97a1a20a8bbb6e1ae20e286ecd5a506f476e7a
smatti <smatti@euclid.univ-lyon1.fr>
2010-07-09 10:08:52 +0200

    sans tcl

--- tcl/maxstrlen.tcl
+++
@@ -1,8 +0,0 @@
-proc maxstrlen {lst {max 0}} {
-##		Find out the length of the longest string in a list
-	foreach i $lst {
-		set l [string length $i]
-		set max [expr $l > $max ? $l : $max]
-	}
-	return $max
-}
