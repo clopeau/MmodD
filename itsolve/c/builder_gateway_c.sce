98187afc49ae7927f119049bdd5143b369c23e4f
ndeffo <ndeffo@euclid.univ-lyon1.fr>
2010-08-30 15:28:42 +0200

    rajout de fortran dans itsolve/, mise en marche de certains builders

---
+++ itsolve/c/builder_gateway_c.sce
@@ -0,0 +1,26 @@
+
+if MSDOS then
+  // to manage long pathname
+  includes_src_c = '-I""' + get_absolute_file_path('builder_gateway_c.sce') + '../../src/c""';
+else
+  includes_src_c = '-I' + get_absolute_file_path('builder_gateway_c.sce') + '../../src/c';
+end
+
+// PutLhsVar managed by user in sci_sum and in sci_sub
+// if you do not this variable, PutLhsVar is added
+// in gateway generated (default mode in scilab 4.x and 5.x)
+WITHOUT_AUTO_PUTLHSVAR = %t;
+functions=['splsolve';'spusolve';'triangular';...
+	'pgmres';'pgc';'pbgc';'pdbgc';'pgcnr';...
+	'pbcgstab';'pqmr'];//'shurgmres';'mshurgmres'];
+
+Files='int'+functions+'.c';
+Files=Files';
+
+tbx_build_gateway('itsolve_c', ['c_'+functions 'int'+functions], [Files], ..
+                  get_absolute_file_path('builder_gateway_c.sce'), ..
+                  ['../../src/c/libSci2spk'],'',includes_src_c);
+
+clear WITHOUT_AUTO_PUTLHSVAR;
+
+clear tbx_build_gateway;
---
+++ itsolve/fortran/inttriangular.c
@@ -0,0 +1,49 @@
+#include <string.h>
+#include "stack-c.h"
+
+extern int inttriangular(char *fname)
+{ 
+  int mA,nA,row=0,i=0,ind=0;
+  int lower=1,upper=1;
+  int un=1;
+  int pstr;
+  SciSparse A;
+
+  CheckRhs(1,1);
+  CheckLhs(1,1);
+  if (VarType(1)==5){
+    GetRhsVar(1,"s",&mA,&nA,&A);    
+    if (mA!=nA){
+      Scierror(501,"%s: input matrix must be square \r\n",fname);
+    }
+  } else {
+    Scierror(501,"%s: input matrix must be sparse \r\n",fname);
+  }
+  
+  while ((row<A.m)&&(lower||upper)) {
+    for(i=0;i<A.mnel[row];i++,ind++){
+      if (A.icol[ind]>row+1)
+	lower=0;
+      if (A.icol[ind]<row+1)
+	upper=0;
+    }
+    row++;
+  }
+
+  if (lower && upper){
+    CreateVar(2,"i",&un,&un,&pstr);
+    *istk(pstr)=3;
+  } else if (lower){ 
+    CreateVar(2,"i",&un,&un,&pstr);
+    *istk(pstr)=1;
+  } else if (upper){
+    CreateVar(2,"i",&un,&un,&pstr);
+    *istk(pstr)=2;
+  } else {
+    CreateVar(2,"i",&un,&un,&pstr);
+    *istk(pstr)=0;
+  }
+  
+  LhsVar(1)=2;
+  return 0;
+}
